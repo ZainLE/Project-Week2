0262f60f2294fcd7e20676265e6eba1db499b92b
ZainLatif7164 <latifzain40@gmail.com>
2024-02-08 02:51:02 +0100

    StreamLit setup complete without map

--- main.py
+++ main.py
@@ -1,40 +1,80 @@
 import openai
 from config import OPENAI_API_KEY
 import streamlit as st
+import uuid
+import random
 
-st.subheader('Tibi , Your travel assistant', divider='blue')
-st.subheader('One click away!')
+# Logo
+logo = "Tibi.png"
+st.image(logo, width=90,)
+
+# Top Lines
+st.subheader("TIBI", divider='blue')
+st.subheader('YOUR OWN TRAVEL ASSISTANT!')
 # st.background()
+with st.chat_message("TIBI"):
+    # avatar= "Tibi.png"
+    # st.image(avatar, width=50, caption="TIBI")
+    st.write("Hi, I'm Tibi, your travel assistant. How can I help you today?")
+
+
+# API KEYS
 
 api_key = OPENAI_API_KEY
-
 openai.api_key = api_key
+# user_input_key = str(uuid.uuid4())
+
+
+# PART OF MESSAGES
+
+questions=["What is destination of choice?","How many days are you planning to travel?","What is Your Budget?","Who do you plan on traveling with on your next adventure?","Which activities are you interested in?"]
+recommended_countries = ["Italy", "France", "Japan", "Spain", "Canada", "Australia", "Brazil", "India", "South Africa", "New Zealand", "Germany", "Mexico", "Thailand", "Greece", "United States"]
+random_country = random.choice(recommended_countries)
+
+
+# INSTRUCTIONS FOR TEXT AND MODEL
 
 def chat_with_gpt(user_input):
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0613",
-        messages=[ 
-            {"role": "system", "content": "You are a travel assistant. Your role is to help users plan trips, collect their preferences,and generate a travel plan. Ensure the generated plan is detailed and user-friendly. After user says greets you,your introductory line is Hello! I'm Tibi, your travel assistant,How can i help you? then after second or third message ask user multiple questions based on their preferences, such as where they would want to go, what kind of activities they like to doon a trip, and their budget. Then, provide them with a personalized trip accordingly "},
-            # {"role": "assistant","content": "Hi, I'm Tibi, your travel assistant. How can I help you today?"},
-            {"role": "user", "content": user_input}, 
+        model="gpt-4-0125-preview",
+          messages=[
+            {
+                "role": "system",
+                "content": f"You are a travel assistant. Your role is to help users plan trips, "
+                           f"collect their preferences, and generate a travel plan. Ensure the generated plan "
+                           f"is detailed and user-friendly. After introductory messages, ask the user the following questions:\n\n"
+                           f"{', '.join(questions)}if user had already answered any of thsoe questions dont ask that question again,when user has dicided on the destination\n\n"
+                           f"give the user itineary for the total travelling he decided for the place he decided on \n\n"
+                           f"If you're looking for recommendations, I can suggest countries like {', '.join(random_country)}."
+            },
+            {"role": "assistant", "content": "Hi, I'm Tibi, your travel assistant. How can I help you today?"},
+            {"role": "user", "content": user_input},
         ],
    
         temperature=0.7,
-        max_tokens=1800
+        max_tokens=3800
     )   
     return response['choices'][0]['message']['content']
 
-user_input = st.chat_input("You: ") # 
 
-while user_input != 'exit':
-   
-    response = chat_with_gpt(user_input)
 
-    print(f"TIBI: {response}")
+# STREAMLIT SETUP 
 
-    user_input = st.text_input("You: ")
+def main():
+    st.subheader("")
+    # st.text("Hello! I'm Tibi, your travel assistant. How can I help you today?")
 
-print("Conversation ended.")
+    # Use st.form to create a form
+    with st.form(key='user_input_form'):
+        user_input = st.text_area("You:", height=30)  
+        submit_button = st.form_submit_button("Submit")
+
+        if submit_button:
+            response = chat_with_gpt(user_input)
+            st.text_area("TIBI:", value=response, height=350, max_chars=None)  
+
+if __name__ == "__main__":
+    main()
 
 
 
